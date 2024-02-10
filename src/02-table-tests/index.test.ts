12d452644522025dcbb3ac4706f53897faf5aa12
dmirty.s <d.strizhakov@gmail.com>
2024-02-10 19:15:39 +0300

    feat: 02-table-tests

--- src/02-table-tests/index.test.ts
+++ src/02-table-tests/index.test.ts
@@ -1,17 +1,41 @@
 // Uncomment the code below and write your tests
-/* import {  simpleCalculator, Action } from './index';
+import { simpleCalculator, Action } from './index';
+
+enum Case {
+  Add = 'should add two numbers',
+  Subtract = 'should subtract two numbers',
+  Divide = 'should divide two numbers',
+  Multiply = 'should multiply two numbers',
+  Exponentiate = 'should exponentiate two numbers',
+  InvalidAction = 'should return null for invalid action',
+  InvalidArgs = 'should return null for invalid arguments',
+}
 
 const testCases = [
-    { a: 1, b: 2, action: Action.Add, expected: 3 },
-    { a: 2, b: 2, action: Action.Add, expected: 4 },
-    { a: 3, b: 2, action: Action.Add, expected: 5 },
-    // continue cases for other actions    
-]; */
+  { a: 1, b: 2, action: Action.Add, expected: 3, case: Case.Add },
+  { a: 7, b: 3, action: Action.Subtract, expected: 4, case: Case.Subtract },
+  { a: 3, b: 2, action: Action.Multiply, expected: 6, case: Case.Subtract },
+  { a: 14, b: 2, action: Action.Divide, expected: 7, case: Case.Divide },
+  {
+    a: 2,
+    b: 3,
+    action: Action.Exponentiate,
+    expected: 8,
+    case: Case.Exponentiate,
+  },
+  { a: 3, b: 2, action: 'invalid', expected: null, case: Case.InvalidAction },
+  {
+    a: '3',
+    b: '2',
+    action: Action.Add,
+    expected: null,
+    case: Case.InvalidArgs,
+  },
+];
 
 describe('simpleCalculator', () => {
-  // This test case is just to run this test suite, remove it when you write your own tests
-  test('should blah-blah', () => {
-    expect(true).toBe(true);
+  test.each(testCases)('$case', ({ a, b, action, expected }) => {
+    const result = simpleCalculator({ a, b, action });
+    expected ? expect(result).toBe(expected) : expect(result).toBeNull();
   });
-  // Consider to use Jest table tests API to test all cases above
 });
