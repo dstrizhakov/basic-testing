a581b21b60a5be8088c778f8e06abe8ccb5ee459
dmirty.s <d.strizhakov@gmail.com>
2024-02-11 12:26:35 +0300

    feat: mocking-lib-api and snapshot testing

--- src/08-snapshot-testing/index.test.ts
+++ src/08-snapshot-testing/index.test.ts
@@ -1,14 +1,30 @@
-// Uncomment the code below and write your tests
-// import { generateLinkedList } from './index';
+import { generateLinkedList } from './index';
 
 describe('generateLinkedList', () => {
   // Check match by expect(...).toStrictEqual(...)
   test('should generate linked list from values 1', () => {
-    // Write your test here
+    const result = generateLinkedList<number>([1, 1, 1, 1]);
+    expect(result).toStrictEqual({
+      value: 1,
+      next: {
+        value: 1,
+        next: {
+          value: 1,
+          next: {
+            value: 1,
+            next: {
+              value: null,
+              next: null,
+            },
+          },
+        },
+      },
+    });
   });
 
   // Check match by comparison with snapshot
   test('should generate linked list from values 2', () => {
-    // Write your test here
+    const result = generateLinkedList<number>([2, 2, 2, 2]);
+    expect(result).toMatchSnapshot();
   });
 });
